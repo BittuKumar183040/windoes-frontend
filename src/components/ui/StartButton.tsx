294f6585a32e4c8120819bc10fb00964d86970c4
Bittu Kumar <bk183040@gmail.com>
2026-01-18 18:15:18 +0530

    fix start button symatric issue

--- src/components/ui/StartButton.tsx
+++ src/components/ui/StartButton.tsx
@@ -2,6 +2,7 @@ import React, { forwardRef, useRef, useImperativeHandle, useState } from "react"
 import { TASKBAR_ICON_RADIUS, TASKBAR_ICON_SIZE_BOUND, TASKBAR_START_LEAF_SIZE } from "../../config/setting";
 import ToolTipNavbar from "./ToolTipNavbar";
 import StartIcon from "./Start/StartIcon";
+import { Circle } from "lucide-react";
 
 type StartButtonProps = {
   active?: boolean;
@@ -34,11 +35,11 @@ const StartButton = forwardRef<HTMLDivElement, StartButtonProps>(({ active, onCl
       onMouseLeave={()=> setShowTip(false)}
       onClick={handleClick}
       style={{
-        width: TASKBAR_ICON_SIZE_BOUND,
+        width: TASKBAR_ICON_SIZE_BOUND-1,
         height: TASKBAR_ICON_SIZE_BOUND,
       }}
-      className={` flex items-center justify-center
-        p-2 rounded overflow-hidden w-fit bg-transparent 
+      className={` flex items-center justify-center shrink-0
+        p-2 rounded overflow-hidden w-fit bg-transparent
         ${active ? "bg-white/30" : "bg-white/0"}
         hover:bg-white/50 transition-colors duration-200 ease-in-out
       `}
